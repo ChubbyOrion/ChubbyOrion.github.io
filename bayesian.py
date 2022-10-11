de09770c25c6990abc4eb63bb98ef96bcd6af6de
fRootSalad <fitered@hackers.best>
2022-10-11 19:46:00 -0400

    more stuff

--- bayesian.py
+++ bayesian.py
@@ -1,67 +1,70 @@
-import itertools
+import csv
 
 import matplotlib.pyplot as plt
-import networkx
 from pomegranate import *
-import pomegranate
-import pandas as pd
-from sys import argv
-import functools
-import matplotlib
-import pygraphviz
-from numpy import nan
-# iinf = argv[1]
-iinf = "door_data/z.csv"
 
-NSTATES = 30
+# Constant values
 
 
-def buildStateNodes() -> [Node]:
-    global NSTATES
-    nods = []
-    dists = []
-    for i in range(NSTATES):
-        distribution = DiscreteDistribution({"L": nan, "R": nan, "S": nan})
-        dists.append(distribution)
-        nod = Node(distribution, name=f"X{i}")
-        nods.append(nod)
-    return nods, dists
-
-
-def direction(val, range):
-    if -range < val < range:
+NSTATES = 15
+def direction(val, rng):
+    if -rng < val < rng:
         return "S"
-    elif val > range:
+    elif val > rng:
         return "R"
     else:
         return "L"
 
 
-def statesFromFile():
-    with open(iinf, 'r') as inf:
-        zs = [float(line.rstrip()) for line in inf]
+def statesFromFile(iinf, n_states = NSTATES, rng = .1):
+    with open("door_data/" + iinf, 'r') as inf:
+        "Read last column of CSV into a list of Z's"
+        reader = csv.reader(inf)
+        row1 = next(reader)
+        tstart = float( row1[3])
+        tfin =float( row1[4])
+        zs = [float(row1[2])]
+        zs += [float(row[2]) for row in reader]
+
     # Create series of NSTATES states from start to finish of data.
 
-    return [[direction(z, range=.02) for z in zs[i:i + NSTATES]] for i in range(len(zs) - NSTATES)]
+    # Plot our points
+    plt.title = iinf
+    plt.scatter(range(len(zs)), zs)
+    plt.figure()
+    statlists = [[direction(z, rng=rng) for z in zs[i:i + n_states]] for i in range(len(zs) - n_states)]
+    # Very lazily assign passedDoor state, should be changed to be manual per run.
 
+    doors = [stat + ([True] if tstart - n_states < num < tfin - n_states else [False]) for num, stat in enumerate(statlists)]
+    return doors
 
+"""Takes in a list of states, number of states to analyze at once, and range to
+ determine "straight" vs right/left velocity """
+def belief(states, n_states = 15 ) -> BayesianNetwork:
 
-stats = statesFromFile()
+    # Flatten state lists to feed them in
+    stats = [item for sublist in states for item in sublist]
 
-stats = [stat + ([False] if num < 230 else [True]) for num, stat in enumerate(stats)]
+    statenames = [f"X{i}" for i in range(n_states)]
 
-statnames = [f"X{i}" for i in range(NSTATES)]
+    # convert list of lists to tuple of tuples to build structure
+    struct = tuple(tuple(sub) for sub in [[n_states] for nstate in range(n_states)] + [[]])
 
+    model = BayesianNetwork.from_structure(stats, struct, state_names=statenames + ["PassedDoor"])
 
-struct = tuple(tuple(sub) for sub in [[NSTATES] for nstate in range(NSTATES) ] + [[]] )
+    model.bake()
 
+    return model
 
-print(struct)
-model = BayesianNetwork.from_structure(stats, struct, state_names=statnames + ["PassedDoor"])
+model = belief(
+    states = [statesFromFile(file, NSTATES) for file in ["1.csv", "2.csv", "3.csv", "4.csv", "5.csv"]])
+# Read in some test States
+testStates = [statesFromFile(file) for file in ["6.csv", "7.csv"]]
 
-print(model.to_json())
+for run in testStates:
+    print("BEGINNING SIMULATION OF A NEW RUN\n\n\n")
+    for state in run:
+        predicted = model.predict([state[:NSTATES] + [None]])[-1][-1]
+        print(f"Predicted: {predicted}  Actual: {state[-1]}")
 
-model.plot()
-matplotlib.pyplot.show()
-
-# buildStates(50)
+        # buildStates(50)
